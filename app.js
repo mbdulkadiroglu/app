6af5358835d88a7434b7795116cba5ad76fac106
mbdulkadiroglu <mbdulkadiroglu@gmail.com>
2023-07-16 02:32:51 +0300

    added chat-gpt support

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
-// These are the dependencies for this file.
-//
-// You installed the `dotenv` and `octokit` modules earlier. The `@octokit/webhooks` is a dependency of the `octokit` module, so you don't need to install it separately. The `fs` and `http` dependencies are built-in Node.js modules.
 import dotenv from "dotenv";
 import {App} from "octokit";
 import {createNodeMiddleware} from "@octokit/webhooks";
 import fs from "fs";
 import http from "http";
+import { Configuration, OpenAIApi } from "openai";
+import axios from "axios";
 
-// This reads your `.env` file and adds the variables from that file to the `process.env` object in Node.js.
 dotenv.config();
 
-// This assigns the values of your environment variables to local variables.
 const appId = process.env.APP_ID;
 const webhookSecret = process.env.WEBHOOK_SECRET;
 const privateKeyPath = process.env.PRIVATE_KEY_PATH;
+const apiKey = process.env.OPENAI_API_KEY;
 
-// This reads the contents of your private key file.
+const configuration = new Configuration({
+    apiKey: apiKey
+});
+const openai = new OpenAIApi(configuration);
+
 const privateKey = fs.readFileSync(privateKeyPath, "utf8");
 
-// This creates a new instance of the Octokit App class.
 const app = new App({
     appId: appId,
     privateKey: privateKey,
@@ -28,18 +29,45 @@ const app = new App({
 });
 
 // This defines the message that your app will post to pull requests.
-const messageForNewPRs = "Thanks for opening a new PR! Please follow our contributing guidelines to make your PR easier to review.";
+const messageForNewPRs = "Review the diff and point out any issues: ";
 
 // This adds an event handler that your code will call later. When this event handler is called, it will log the event to the console. Then, it will use GitHub's REST API to add a comment to the pull request that triggered the event.
 async function handlePullRequestOpened({octokit, payload}) {
     console.log(`Received a pull request event for #${payload.pull_request.number}`);
 
+
     try {
+        const diff_url = payload.pull_request.diff_url;
+        const diff = await axios.get(diff_url);
+        const diffText = diff.data;
+
+        const gptResponse = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages:[
+                {
+                    "role" : "system",
+                    "content" : "You will be reviewing a diff of a pull request. Please review it and point out any issues in a concise way."
+                },
+                {
+                    "role" : "user",
+                    "content" : diffText
+                }
+            ],
+            maxTokens: 256,
+            temperature: 0,
+            topP: 1,
+            presencePenalty: 0,
+            frequencyPenalty: 0,
+        })
+
+        const message = gptResponse.data.choices[0].text;
+        console.log(message);
+
         await octokit.request("POST /repos/{owner}/{repo}/issues/{issue_number}/comments", {
             owner: payload.repository.owner.login,
             repo: payload.repository.name,
             issue_number: payload.pull_request.number,
-            body: messageForNewPRs,
+            body: message,
             headers: {
                 "x-github-api-version": "2022-11-28",
             },
@@ -50,7 +78,7 @@ async function handlePullRequestOpened({octokit, payload}) {
         }
         console.error(error)
     }
-};
+}
 
 // This sets up a webhook event listener. When your app receives a webhook event from GitHub with a `X-GitHub-Event` header value of `pull_request` and an `action` payload value of `opened`, it calls the `handlePullRequestOpened` event handler that is defined above.
 app.webhooks.on("pull_request.opened", handlePullRequestOpened);
